c360da4fedf898d24e760eed372420f0e3205e96
Joaquim Perez <jivkim@gmail.com>
2025-11-25 16:17:19 +0100

    remove spotify stuff from front-end, we will use spotify web app as player, and all requests from API

--- vue-app/views/Home.vue
+++ vue-app/views/Home.vue
@@ -1,48 +1,5 @@
-<script setup>
-import { useMainStore } from "../stores/main";
-
-const mainStore = useMainStore();
-
-window.onSpotifyWebPlaybackSDKReady = () => {
-    console.log("onSpotifyWebPlaybackSDKReady");
-
-    if (!mainStore.spotifyToken?.access_token) {
-        console.warn("No Spotify access token");
-        return;
-    }
-
-    const player = new Spotify.Player({
-        name: "Web Playback SDK Quick Start Player",
-        getOAuthToken: (cb) => {
-            cb(mainStore.spotifyToken?.access_token);
-        },
-        volume: 0.5,
-    });
-
-    player.addListener("ready", ({ device_id }) => {
-        console.log("Ready with Device ID", device_id);
-    });
-
-    player.addListener("not_ready", ({ device_id }) => {
-        console.log("Device ID has gone offline", device_id);
-    });
-
-    player.addListener("initialization_error", ({ message }) => {
-        console.error(message);
-    });
-
-    player.addListener("authentication_error", ({ message }) => {
-        console.error(message);
-    });
-
-    player.addListener("account_error", ({ message }) => {
-        console.error(message);
-    });
-
-    player.connect();
-};
-</script>
+<script setup></script>
 
 <template>
-    <div></div>
+    <div>Home</div>
 </template>
