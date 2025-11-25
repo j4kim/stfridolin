70592b38ea7257b04097174d0b5ee32df258b58d
Joaquim Perez <jivkim@gmail.com>
2025-11-26 00:23:52 +0100

    restore route to get devices

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\SpotifyController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('spotify/devices', [SpotifyController::class, 'devices'])->name('spotify.devices');
     Route::get('spotify/playback-state', [SpotifyController::class, 'playbackState'])->name('spotify.playback-state');
     Route::put('spotify/play', [SpotifyController::class, 'play'])->name('spotify.play');
     Route::put('spotify/pause', [SpotifyController::class, 'pause'])->name('spotify.pause');
