0c18f542091a0ba37a9e1f755683366797700b8a
Joaquim Perez <jivkim@gmail.com>
2025-12-02 11:18:17 +0100

    layout(Spotify)

--- vue-app/views/Spotify.vue
+++ vue-app/views/Spotify.vue
@@ -50,60 +50,62 @@ async function selectDevice(deviceId) {
 </script>
 
 <template>
-    <div v-if="error">
-        <p v-if="error === 'App\\Exceptions\\NoSpotifyTokenException'">
-            No Spotify Token.
-            <a
-                :href="ziggy('spotify-login', { intended: route.path })"
-                class="link-primary"
-            >
-                Login to Spotify
-            </a>
-        </p>
-        <p v-if="error === 'App\\Exceptions\\NoSpotifyPlaybackException'">
-            No spotify playback.
-            <a
-                href="https://open.spotify.com"
-                target="_blank"
-                class="link-primary"
-            >
-                Open Spotify
-            </a>
-        </p>
-    </div>
-
-    <div class="devices">
-        Devices:
-        <div v-for="device in devices" class="flex gap-2">
-            <span class="font-semibold">{{ device.name }}</span>
-            <span>{{ device.is_active ? "active" : "inactive" }}</span>
-            <button
-                class="btn btn-xs btn-soft btn-primary"
-                :class="{ 'btn-disabled': device.selected }"
-                @click="selectDevice(device.id)"
-                v-text="device.selected ? 'selected' : 'select'"
-            ></button>
+    <div class="flex flex-col gap-8">
+        <div v-if="error">
+            <p v-if="error === 'App\\Exceptions\\NoSpotifyTokenException'">
+                No Spotify Token.
+                <a
+                    :href="ziggy('spotify-login', { intended: route.path })"
+                    class="link-primary"
+                >
+                    Login to Spotify
+                </a>
+            </p>
+            <p v-if="error === 'App\\Exceptions\\NoSpotifyPlaybackException'">
+                No spotify playback.
+                <a
+                    href="https://open.spotify.com"
+                    target="_blank"
+                    class="link-primary"
+                >
+                    Open Spotify
+                </a>
+            </p>
         </div>
-    </div>
 
-    <div v-if="playback">
-        <img
-            class="inline"
-            v-if="playback.item.album.images.length"
-            :src="
-                playback.item.album.images[
-                    playback.item.album.images.length - 1
-                ].url
-            "
-        />
-        {{ playback.item.artists[0].name }} - {{ playback.item.name }}
-        <div class="flex items-center gap-1">
-            <progress
-                :max="playback.item.duration_ms"
-                :value="playback.progress_ms"
-            ></progress>
-            <button v-if="is_playing" @click="pause">⏸️</button>
-            <button v-else @click="play">▶️</button>
+        <div class="devices">
+            Devices:
+            <div v-for="device in devices" class="flex gap-2">
+                <span class="font-semibold">{{ device.name }}</span>
+                <span>{{ device.is_active ? "active" : "inactive" }}</span>
+                <button
+                    class="btn btn-xs btn-soft btn-primary"
+                    :class="{ 'btn-disabled': device.selected }"
+                    @click="selectDevice(device.id)"
+                    v-text="device.selected ? 'selected' : 'select'"
+                ></button>
+            </div>
+        </div>
+
+        <div v-if="playback">
+            <img
+                class="inline"
+                v-if="playback.item.album.images.length"
+                :src="
+                    playback.item.album.images[
+                        playback.item.album.images.length - 1
+                    ].url
+                "
+            />
+            {{ playback.item.artists[0].name }} - {{ playback.item.name }}
+            <div class="flex items-center gap-1">
+                <progress
+                    :max="playback.item.duration_ms"
+                    :value="playback.progress_ms"
+                ></progress>
+                <button v-if="is_playing" @click="pause">⏸️</button>
+                <button v-else @click="play">▶️</button>
+            </div>
         </div>
     </div>
 </template>
