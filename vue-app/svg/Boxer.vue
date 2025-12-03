a9dc9cc5d3947009d5b4e60a158b27e9ddcf4a78
Joaquim Perez <jivkim@gmail.com>
2025-12-03 18:35:47 +0100

    feat(Boxers): animate paths

--- vue-app/svg/Boxer.vue
+++ vue-app/svg/Boxer.vue
@@ -23,8 +23,13 @@ const root = useTemplateRef("root");
 onMounted(() => {
     const duration = animBodyDuration;
     const ease = "power1.inOut"; // "back.inOut" is cool too
-    const animables = root.value.querySelectorAll("path, use");
-    // todo
+    const animables = root.value.querySelectorAll("path");
+    Array.from(animables).forEach((el) => {
+        const toEl = document.querySelector(
+            `#frame-2 [data-name=${el.dataset.name}]`,
+        );
+        gsap.to(el, { duration, morphSVG: toEl, repeat: -1, yoyo: true, ease });
+    });
 });
 </script>
 
