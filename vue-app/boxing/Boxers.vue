4fd86f1893177caa8db0ea4350816c0aab0fb388
Joaquim Perez <jivkim@gmail.com>
2025-12-04 16:37:38 +0100

    [wip] refactor(Boxers): code intentions

--- vue-app/boxing/Boxers.vue
+++ vue-app/boxing/Boxers.vue
@@ -1,76 +1,29 @@
 <script setup>
-import { ref } from "vue";
 import Boxer from "./Boxer.vue";
-import boxer1 from "./svg/boxer-1.svg?raw";
-import boxer2 from "./svg/boxer-2.svg?raw";
-import boxer3 from "./svg/boxer-3.svg?raw";
+import { useBoxingStore } from "../stores/boxing";
 
-const parser = new DOMParser();
-
-function getBoxerSvgContent(svg, id) {
-    const dom = parser.parseFromString(svg, "application/xml");
-    const rootG = dom.querySelector("g");
-    Array.from(rootG.children).forEach((g) => {
-        g.dataset.name = g.id;
-        g.id = `${id}-${g.id}`;
-        Array.from(g.children).forEach((el, index) => {
-            el.id = `${g.id}-${index}`;
-            el.dataset.name = `${g.dataset.name}-${index}`;
-        });
-    });
-    return rootG.innerHTML;
-}
-
-const framesXml = [
-    { id: "frame-1", svg: boxer1 },
-    { id: "frame-2", svg: boxer2 },
-    { id: "frame-3", svg: boxer3 },
-].map(({ id, svg }) => {
-    return { id, content: getBoxerSvgContent(svg, id) };
-});
-
-const imgUrls = ref({
-    left: "https://i.scdn.co/image/ab67616d0000b27389b9e48b79603248d4fea627",
-    right: "https://i.scdn.co/image/ab67616d0000b2734e09836e2d1938337c416bf2",
-});
+const boxingStore = useBoxingStore();
 </script>
 
 <template>
     <div>
         <svg class="max-h-[80vh] w-full" viewBox="0 0 1500 1000">
-            <defs>
-                <g
-                    v-for="svg in framesXml"
-                    :id="svg.id"
-                    v-html="svg.content"
-                ></g>
-                <image
-                    id="_Image1"
-                    width="4px"
-                    height="4px"
-                    :href="imgUrls.left"
-                />
-                <image
-                    id="_Image2"
-                    width="4px"
-                    height="4px"
-                    :href="imgUrls.right"
-                />
-            </defs>
-            <Boxer
-                id="boxer-left"
-                :initialSvgContent="getBoxerSvgContent(boxer1, 'boxer-left')"
-            />
-            <Boxer
-                id="boxer-right"
-                reversed
-                :initialSvgContent="
-                    getBoxerSvgContent(boxer1, 'boxer-right').replace(
-                        '_Image1',
-                        '_Image2',
-                    )
-                "
-            />
+            <template v-for="boxer in boxingStore.boxers">
+                <defs>
+                    <g
+                        v-for="svg in boxer.svgFrames"
+                        :id="svg.id"
+                        v-html="svg.content"
+                    ></g>
+                    <image
+                        :id="boxer.imageId"
+                        width="4px"
+                        height="4px"
+                        :href="boxer.imgUrl"
+                    />
+                </defs>
+                <Boxer id="boxer-left" :boxer />
+            </template>
         </svg>
     </div>
 </template>
