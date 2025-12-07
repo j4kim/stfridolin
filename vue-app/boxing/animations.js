277fdf15d0de4c58e2b6b0aeb618f90083dfba92
Joaquim Perez <jivkim@gmail.com>
2025-12-07 22:54:50 +0000

    refactor(animations): reusable addSubTimeline

--- vue-app/boxing/animations.js
+++ vue-app/boxing/animations.js
@@ -4,32 +4,34 @@ import { getShapeIndex } from "./utils";
 export class Sway {
     constructor(fighter) {
         this.fighter = fighter;
-        this.tl = gsap.timeline({ repeat: -1, yoyo: true });
-        this.fromFrameId = "base1";
-        this.toFrameId = "base2";
-        this.duration = 1;
-        this.ease = "power1.inOut";
-        this.addTweens();
+        this.baseTl = gsap.timeline({ repeat: -1, yoyo: true });
+        this.addSubTimeline("base1", "base2", 1, "power1.inOut");
     }
 
-    addTweens() {
+    addSubTimeline(fromFrame, toFrame, duration, ease) {
+        const tl = gsap.timeline();
         this.fighter.animables.forEach((el) => {
-            const toSel = `#${this.fighter.id}_${this.toFrameId} [data-name=${el.dataset.name}]`;
+            const toSel = `#${this.fighter.id}_${toFrame} [data-name=${el.dataset.name}]`;
             const toEl = document.querySelector(toSel);
-            const vars = { ease: this.ease, duration: this.duration };
+            if (!toEl) {
+                console.warn("toEl not found", toSel);
+                return;
+            }
+            const vars = { ease, duration };
             if (el.nodeName === "path") {
                 vars.morphSVG = {
                     shape: toEl,
                     shapeIndex: getShapeIndex(
-                        this.fromFrameId,
-                        this.toFrameId,
+                        fromFrame,
+                        toFrame,
                         el.dataset.name,
                     ),
                 };
             } else if (el.nodeName === "use") {
                 vars.transform = toEl.attributes.transform.value;
             }
-            this.tl.to(el, vars, 0);
+            tl.to(el, vars, 0);
         });
+        this.baseTl.add(tl);
     }
 }
