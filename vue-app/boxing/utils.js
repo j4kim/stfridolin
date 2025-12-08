4f0aea82be486395489416b9d3aba7f6d0764b05
Joaquim Perez <jivkim@gmail.com>
2025-12-08 20:39:09 +0000

    move addToTl inside Fighter

--- vue-app/boxing/utils.js
+++ vue-app/boxing/utils.js
@@ -33,31 +33,6 @@ export function getShapeIndex(fromFrame, toFrame, name) {
     return morphShapeIndexes[`${fromFrame}-${toFrame}`]?.[name];
 }
 
-export function addToTl(
-    animables,
-    tl,
-    fromFrame,
-    toFrame,
-    duration,
-    ease = "power1.inOut",
-    position = 0,
-) {
-    animables.forEach((el) => {
-        const toSel = `#frame-${toFrame} [data-name=${el.dataset.name}]`;
-        const toEl = document.querySelector(toSel);
-        const vars = { ease, duration };
-        if (el.nodeName === "path") {
-            vars.morphSVG = {
-                shape: toEl,
-                shapeIndex: getShapeIndex(fromFrame, toFrame, el.dataset.name),
-            };
-        } else if (el.nodeName === "use") {
-            vars.transform = toEl.attributes.transform.value;
-        }
-        tl.to(el, vars, position);
-    });
-}
-
 export class Fighter {
     constructor(id) {
         this.id = id;
@@ -72,11 +47,38 @@ export class Fighter {
         this.punchOutDuration = 0.5;
     }
 
+    addToTl(
+        tl,
+        fromFrame,
+        toFrame,
+        duration,
+        ease = "power1.inOut",
+        position = 0,
+    ) {
+        this.animables.forEach((el) => {
+            const toSel = `#frame-${toFrame} [data-name=${el.dataset.name}]`;
+            const toEl = document.querySelector(toSel);
+            const vars = { ease, duration };
+            if (el.nodeName === "path") {
+                vars.morphSVG = {
+                    shape: toEl,
+                    shapeIndex: getShapeIndex(
+                        fromFrame,
+                        toFrame,
+                        el.dataset.name,
+                    ),
+                };
+            } else if (el.nodeName === "use") {
+                vars.transform = toEl.attributes.transform.value;
+            }
+            tl.to(el, vars, position);
+        });
+    }
+
     mount() {
         const root = document.getElementById(this.id);
         this.animables = root.querySelectorAll("path, use");
-        addToTl(
-            this.animables,
+        this.addToTl(
             this.baseTl,
             1,
             2,
@@ -84,17 +86,8 @@ export class Fighter {
             "power1.inOut",
             0,
         );
-        addToTl(
-            this.animables,
-            this.punchTl,
-            1,
-            3,
-            this.punchInDuration,
-            "back.in(3)",
-            0,
-        );
-        addToTl(
-            this.animables,
+        this.addToTl(this.punchTl, 1, 3, this.punchInDuration, "back.in(3)", 0);
+        this.addToTl(
             this.punchTl,
             3,
             1,
