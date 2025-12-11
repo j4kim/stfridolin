5e8a079451196b9e8a7943e2c4154efa64fe42e6
Joaquim Perez <jivkim@gmail.com>
2025-12-11 19:24:00 +0100

    add right boxer + image (image is inverted and arms move funny)

--- vue-app/stores/boxing.js
+++ vue-app/stores/boxing.js
@@ -4,7 +4,7 @@ import { Fighter } from "../boxing/Fighter";
 export const useBoxingStore = defineStore("boxing", () => {
     const fighters = {
         left: new Fighter("left"),
-        // right: new Boxer("right"),
+        right: new Fighter("right"),
     };
 
     return { fighters };
