bcd6987a08e4c607f3c943a5307f9ba36495b88b
Joaquim Perez <jivkim@gmail.com>
2025-12-11 18:59:24 +0100

    fix(utils): review path names + getShapeIndex returns 0 if not defined

--- vue-app/boxing/utils.js
+++ vue-app/boxing/utils.js
@@ -1,34 +1,16 @@
 const morphShapeIndexes = {
-    "base1-punch": {
-        "leg_back-0": 0,
-        "leg_back-1": 0,
-        "arm_back-0": 0,
-        "arm_back-1": 0,
-        "arm_back-2": 0,
-        "leg_front-0": 0,
-        "leg_front-1": 6,
-        "torso-0": 0,
-        "arm_front-0": 7,
-        "arm_front-1": 0,
-        "arm_front-2": 0,
+    "base2-punch": {
+        arm_front_0: 7,
     },
     "punch-base2": {
-        "leg_back-0": 0,
-        "leg_back-1": 0,
-        "arm_back-0": 0,
-        "arm_back-1": 0,
-        "arm_back-2": 0,
-        "leg_front-0": 0,
-        "leg_front-1": 0,
-        "torso-0": 0,
-        "arm_front-0": 1,
-        "arm_front-1": 0,
-        "arm_front-2": 0,
+        arm_front_0: 1,
     },
 };
 
 export function getShapeIndex(fromFrame, toFrame, name) {
-    return morphShapeIndexes[`${fromFrame}-${toFrame}`]?.[name];
+    const couple = `${fromFrame}-${toFrame}`;
+    const index = morphShapeIndexes[couple]?.[name] ?? 0;
+    return index;
 }
 
 export function random(min, max) {
