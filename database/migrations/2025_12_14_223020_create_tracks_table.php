48ab2a3efcde83b39ba1385808f7c8d09091e248
Joaquim Perez <jivkim@gmail.com>
2025-12-14 22:59:33 +0100

    add proposed_by fk in tracks table

--- database/migrations/2025_12_14_223020_create_tracks_table.php
+++ database/migrations/2025_12_14_223020_create_tracks_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('tracks', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('proposed_by')->nullable()->constrained('guests');
             $table->json('spotify_data');
             $table->unsignedTinyInteger('priority')->default(0);
             $table->boolean('won')->nullable();
