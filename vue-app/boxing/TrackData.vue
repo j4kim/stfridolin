b9d02695c0e24cefd162ec4ea8a5f61edb994e6b
Joaquim Perez <jivkim@gmail.com>
2025-12-15 20:30:41 +0100

    refactor: new TrackData

---
+++ vue-app/boxing/TrackData.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { useBoxingStore } from "../stores/boxing";
+
+defineProps({
+    track: Object,
+});
+
+const boxingStore = useBoxingStore();
+
+boxingStore.fetchCurrentFight();
+</script>
+
+<template>
+    <div class="p-[1vw] pt-[4vw] text-center">
+        <div class="text-[2.2vw] font-semibold">
+            {{ track.name }}
+        </div>
+        <div class="text-[2.2vw] font-light">
+            {{ track.artist_name }}
+        </div>
+        <div class="text-[10vw] leading-none font-bold">2</div>
+        <div class="text-[2.2vw] font-bold">votes</div>
+    </div>
+</template>
--- vue-app/views/Boxing.vue
+++ vue-app/views/Boxing.vue
@@ -3,6 +3,7 @@ import Boxers from "../boxing/Boxers.vue";
 import ring from "../boxing/svg/ring.svg";
 import BoxingControls from "../boxing/BoxingControls.vue";
 import { useBoxingStore } from "../stores/boxing";
+import TrackData from "../boxing/TrackData.vue";
 
 const boxingStore = useBoxingStore();
 
@@ -19,29 +20,17 @@ boxingStore.fetchCurrentFight();
     >
         <div class="aspect-240/35"></div>
         <div class="flex grow">
-            <div class="-mr-[6vw] grow p-[1vw] text-center">
-                <div class="text-[2.2vw] font-semibold">
-                    {{ boxingStore.fight.left_track.name }}
-                </div>
-                <div class="text-[2.2vw] font-light">
-                    {{ boxingStore.fight.left_track.artist_name }}
-                </div>
-                <div class="text-[10vw] leading-none font-bold">2</div>
-                <div class="text-[2.2vw] font-bold">votes</div>
-            </div>
+            <TrackData
+                class="-mr-[6vw] grow"
+                :track="boxingStore.fight.left_track"
+            />
             <div class="aspect-3/2 h-full">
                 <Boxers />
             </div>
-            <div class="-ml-[6vw] grow p-[1vw] text-center">
-                <div class="text-[2.2vw] font-semibold">
-                    {{ boxingStore.fight.right_track.name }}
-                </div>
-                <div class="text-[2.2vw] font-light">
-                    {{ boxingStore.fight.right_track.artist_name }}
-                </div>
-                <div class="text-[10vw] leading-none font-bold">9</div>
-                <div class="text-[2.2vw] font-bold">votes</div>
-            </div>
+            <TrackData
+                class="-ml-[6vw] grow"
+                :track="boxingStore.fight.right_track"
+            />
         </div>
     </div>
     <BoxingControls class="mt-2" />
