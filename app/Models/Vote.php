1a7911cf828d6e33e5ccc9576c36dd77641d09f6
Joaquim Perez <jivkim@gmail.com>
2025-12-16 17:02:03 +0100

    Vote: broadcast events on votes channel

--- app/Models/Vote.php
+++ app/Models/Vote.php
@@ -2,11 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Database\Eloquent\BroadcastsEvents;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Vote extends Model
 {
+    use BroadcastsEvents;
+
     public function guest(): BelongsTo
     {
         return $this->belongsTo(Guest::class);
@@ -20,5 +24,10 @@ class Vote extends Model
     public function fight(): BelongsTo
     {
         return $this->belongsTo(Fight::class);
+    }
+
+    public function broadcastOn(string $event): array
+    {
+        return [new Channel('votes')];
     }
 }
