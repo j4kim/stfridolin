1214b66a634adc6ede547e465fb678ee7c9d61e2
Joaquim Perez <jivkim@gmail.com>
2025-12-16 14:33:24 +0100

    layout(CurrentTrack): keep layout when there are no playing track

--- vue-app/boxing/CurrentTrack.vue
+++ vue-app/boxing/CurrentTrack.vue
@@ -11,23 +11,18 @@ spotify.getPlaybackState();
         <div class="w-1/5 text-right text-[1.6vw]">
             À l'écoute actuellement:
         </div>
-        <div
-            class="card card-side card-md w-4/5 bg-black/60"
-            v-if="spotify.track"
-        >
-            <figure>
-                <img
-                    :src="spotify.track.img_url"
-                    class="aspect-square h-[7.5vw]"
-                />
+        <div class="card card-side card-md grow bg-black/60">
+            <figure class="aspect-square h-[7.5vw]">
+                <img v-if="spotify.track" :src="spotify.track.img_url" />
             </figure>
             <div class="card-body p-[1.8vw]">
-                <p class="text-[1.4vw]">
+                <p class="text-[1.4vw]" v-if="spotify.track">
                     <span class="font-bold">{{ spotify.track.name }}</span>
                     -
                     <span>{{ spotify.track.artist_name }}</span>
                 </p>
                 <progress
+                    v-if="spotify.track"
                     class="progress h-[0.8vw]"
                     :value="spotify.progressRatio"
                 ></progress>
