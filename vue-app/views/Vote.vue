dabd882b681e5690ecfcc45665673619747e0afb
Joaquim Perez <jivkim@gmail.com>
2025-12-19 15:16:43 +0100

    Vote.vue: use Item shadcn component

--- vue-app/views/Vote.vue
+++ vue-app/views/Vote.vue
@@ -4,6 +4,12 @@ import { post } from "@/api";
 import { useMainStore } from "@/stores/main";
 import { useFightStore } from "@/stores/fight";
 import Button from "@/components/ui/button/Button.vue";
+import {
+    Item,
+    ItemContent,
+    ItemActions,
+    ItemMedia,
+} from "@/components/ui/item";
 
 const mainStore = useMainStore();
 
@@ -39,29 +45,31 @@ async function vote() {
         {{ mainStore.appName }}
     </h1>
     <div class="my-2 px-4 font-bold">Combat en cours</div>
-    <ul class="list rounded-box shadow-md" v-if="fightStore.fight">
-        <li
+    <div class="flex flex-col border-t" v-if="fightStore.fight">
+        <Item
             v-for="track in [
                 fightStore.fight.left_track,
                 fightStore.fight.right_track,
             ]"
-            class="list-row"
+            class="border-border rounded-none border-0 border-b"
         >
-            <div>
+            <ItemMedia>
                 <img
                     class="rounded-box size-10"
                     :src="track.img_thumbnail_url"
                 />
-            </div>
-            <div>
+            </ItemMedia>
+            <ItemContent>
                 <div>{{ track.name }}</div>
                 <div class="text-xs font-semibold opacity-60">
                     {{ track.artist_name }}
                 </div>
-            </div>
-            <Button @click="select(track)"> Voter </Button>
-        </li>
-    </ul>
+            </ItemContent>
+            <ItemActions>
+                <Button @click="select(track)"> Voter </Button>
+            </ItemActions>
+        </Item>
+    </div>
 
     <div class="mt-8 px-4">
         <Button class="w-full"> Ajouter un morceau en file d'attente </Button>
