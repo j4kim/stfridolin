1418a378a639482e673bcb584864ef25b3a01257
Joaquim Perez <jivkim@gmail.com>
2025-12-21 00:34:04 +0100

    feat(Boxing): allow to show ring in fullscreen

--- vue-app/views/Boxing.vue
+++ vue-app/views/Boxing.vue
@@ -1,10 +1,19 @@
 <script setup>
 import Ring from "@/boxing/Ring.vue";
 import Layout from "@/components/Layout.vue";
+import { Button } from "@/components/ui/button";
+import { useFullscreen } from "@vueuse/core";
+import { useTemplateRef } from "vue";
+
+const ring = useTemplateRef("ring");
+const { isFullscreen, enter } = useFullscreen(ring);
 </script>
 
 <template>
     <Layout>
-        <Ring />
+        <Ring ref="ring" :class="{ 'cursor-none': isFullscreen }" />
+        <div class="p-2">
+            <Button @click="enter">Plein écran</Button>
+        </div>
     </Layout>
 </template>
