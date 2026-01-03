012f867c9fd483262d37fca3414ab00310e2ca11
Joaquim Perez <jivkim@gmail.com>
2026-01-03 23:42:37 +0100

    fix(VoteController): prevent vote for an ended fight

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers;
 use App\Models\Fight;
 use App\Models\Track;
 use App\Models\Vote;
+use Exception;
 use Illuminate\Http\Request;
 
 class VoteController extends Controller
 {
     public function vote(Fight $fight, Track $track)
     {
+        if ($fight->ended_at) {
+            throw new Exception("This fight is ended");
+        }
         Vote::create([
             'track_id' => $track->id,
             'fight_id' => $fight->id,
