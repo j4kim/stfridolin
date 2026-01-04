8b181051fcd7cab1d78f60fa62b98df72a740aab
Joaquim Perez <jivkim@gmail.com>
2026-01-04 18:58:36 +0100

    layout(Ring): keep aspect ratio for ring, with margin top and bottom in fullscreen if screen is narrower, doesn't work if screen is wider :/

--- vue-app/boxing/Ring.vue
+++ vue-app/boxing/Ring.vue
@@ -13,41 +13,43 @@ fightStore.fetchCurrentFight().then(boxingStore.run);
 </script>
 
 <template>
-    <div
-        class="@container flex aspect-video flex-col"
-        :style="{
-            backgroundImage: `url(${ring})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat',
-        }"
-    >
-        <div class="flex aspect-240/35 flex-col">
-            <h1
-                class="text-center text-[2cqw] font-bold tracking-widest uppercase"
-            >
-                <RouterLink to="/"> Jukeboxe </RouterLink>
-            </h1>
-            <CurrentTrack class="grow" />
-            <div class="text-center text-[1.6cqw]">
-                Combat pour le prochain morceau
-            </div>
-        </div>
-        <div class="flex grow" v-if="fightStore.fight">
-            <TrackData
-                class="-mr-[8cqw] w-[26cqw]"
-                :track="fightStore.fight.left_track"
-            />
-            <div class="relative flex grow-4 flex-col">
-                <Boxers class="grow" />
-                <div class="w-full text-center text-[1.3cqw]">
-                    Utilisez l'application pour ajouter un concurrent
+    <div class="flex items-center">
+        <div
+            class="@container flex aspect-video w-full flex-col"
+            :style="{
+                backgroundImage: `url(${ring})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+            }"
+        >
+            <div class="flex aspect-240/35 flex-col">
+                <h1
+                    class="text-center text-[2cqw] font-bold tracking-widest uppercase"
+                >
+                    <RouterLink to="/"> Jukeboxe </RouterLink>
+                </h1>
+                <CurrentTrack class="grow" />
+                <div class="text-center text-[1.6cqw]">
+                    Combat pour le prochain morceau
                 </div>
             </div>
-            <TrackData
-                class="-ml-[8cqw] w-[26cqw]"
-                :track="fightStore.fight.right_track"
-            />
+            <div class="flex grow" v-if="fightStore.fight">
+                <TrackData
+                    class="-mr-[8cqw] w-[26cqw]"
+                    :track="fightStore.fight.left_track"
+                />
+                <div class="relative flex grow-4 flex-col">
+                    <Boxers class="grow" />
+                    <div class="w-full text-center text-[1.3cqw]">
+                        Utilisez l'application pour ajouter un concurrent
+                    </div>
+                </div>
+                <TrackData
+                    class="-ml-[8cqw] w-[26cqw]"
+                    :track="fightStore.fight.right_track"
+                />
+            </div>
         </div>
     </div>
 </template>
