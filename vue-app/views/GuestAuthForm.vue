3fb04dfbc814bf1528d24f2a69c3c81ad738d3d7
Joaquim Perez <jivkim@gmail.com>
2026-01-26 22:41:54 +0100

    feat(GuestAuthForm): add form for guest code

--- vue-app/views/GuestAuthForm.vue
+++ vue-app/views/GuestAuthForm.vue
@@ -1,7 +1,53 @@
 <script setup>
 import Layout from "@/components/Layout.vue";
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import {
+    InputOTP,
+    InputOTPGroup,
+    InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { ref } from "vue";
+import { REGEXP_ONLY_DIGITS_AND_CHARS } from "vue-input-otp";
+
+const value = ref("");
 </script>
 
 <template>
-    <Layout simple> Guest auth form </Layout>
+    <Layout simple>
+        <Card class="mx-auto mt-4 w-full max-w-sm">
+            <CardHeader>
+                <CardTitle>Authentification</CardTitle>
+                <CardDescription>
+                    Entre ton code de 4 caractères
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <form>
+                    <InputOTP
+                        v-model="value"
+                        :maxlength="4"
+                        :pattern="REGEXP_ONLY_DIGITS_AND_CHARS"
+                    >
+                        <InputOTPGroup class="mx-auto">
+                            <InputOTPSlot :index="0" />
+                            <InputOTPSlot :index="1" />
+                            <InputOTPSlot :index="2" />
+                            <InputOTPSlot :index="3" />
+                        </InputOTPGroup>
+                    </InputOTP>
+                </form>
+            </CardContent>
+            <CardFooter class="flex flex-col gap-2">
+                <Button class="w-full"> Valider </Button>
+            </CardFooter>
+        </Card>
+    </Layout>
 </template>
