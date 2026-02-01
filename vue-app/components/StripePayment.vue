11025b296280fd13b8522047682649c183d142b3
Joaquim Perez <jivkim@gmail.com>
2026-02-01 17:13:41 +0100

    feat(StripePayment): show error message

--- vue-app/components/StripePayment.vue
+++ vue-app/components/StripePayment.vue
@@ -1,12 +1,16 @@
 <script setup>
 import { loadStripe } from "@stripe/stripe-js";
-import { onMounted, useTemplateRef } from "vue";
+import { onMounted, ref, useTemplateRef } from "vue";
 import { Button } from "./ui/button";
+import { Alert, AlertDescription } from "./ui/alert";
+import { TriangleAlert } from "lucide-vue-next";
 import { route } from "../../vendor/tightenco/ziggy";
 
 const props = defineProps({
     intent: Object,
 });
+
+const err = ref("");
 
 const stripe = await loadStripe(import.meta.env.VITE_STRIPE_PK);
 
@@ -36,23 +40,31 @@ onMounted(() => {
 });
 
 async function submit() {
-    await stripe.confirmPayment({
+    err.value = "";
+    const { error } = await stripe.confirmPayment({
         elements,
         confirmParams: {
             return_url: route("payments.stripe-callback"),
         },
     });
+    err.value = error.message;
 }
 </script>
 
 <template>
-    <form class="mb-8 flex flex-col gap-2" @submit.prevent="submit">
+    <form class="mb-8 flex flex-col gap-2 px-4" @submit.prevent="submit">
         <div>Achat de {{ intent.metadata.tokens }} jetons</div>
         <div class="text-xl">
             Total: <span class="font-bold">{{ intent.metadata.chf }} CHF</span>
         </div>
         <hr />
         <div ref="paymentContainer"></div>
+        <Alert class="mt-4" variant="destructive" v-if="err">
+            <TriangleAlert />
+            <AlertDescription>
+                {{ err }}
+            </AlertDescription>
+        </Alert>
         <hr />
         <Button type="submit">Continuer</Button>
     </form>
