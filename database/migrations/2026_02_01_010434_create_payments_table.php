b4fca8210e7da14a15348dd9aec3d8690dc0294f
Joaquim Perez <jivkim@gmail.com>
2026-02-01 01:08:02 +0100

    create payments table migration and Payment model

---
+++ database/migrations/2026_02_01_010434_create_payments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('guest_id')->constrained();
+            $table->string('stripe_id')->unique();
+            $table->json('stripe_data');
+            $table->string('purpose')->nullable();
+            $table->integer('amount');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
