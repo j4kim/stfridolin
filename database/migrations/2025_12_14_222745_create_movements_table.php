ab4c07a5a791e3ab33e6ecb3ff9bf60bfb96d3d9
Joaquim Perez <jivkim@gmail.com>
2026-02-02 17:04:45 +0100

    migrations(movements): add guest_id and payment_id fks

--- database/migrations/2025_12_14_222745_create_movements_table.php
+++ database/migrations/2025_12_14_222745_create_movements_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('movements', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('guest_id')->constrained();
+            $table->foreignId('payment_id')->nullable()->constrained();
             $table->integer('amount');
             $table->string('type');
             $table->json('meta')->nullable();
