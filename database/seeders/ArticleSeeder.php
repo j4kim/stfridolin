8ca4c45f536af9b6761ec60bf59bdf0b3e2d1539
Joaquim Perez <jivkim@gmail.com>
2026-02-02 19:13:20 +0100

    feat(ArticleSeeder): create token packages

---
+++ database/seeders/ArticleSeeder.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Article;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ArticleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $tokenPackages = [
+            [
+                'tokens' => 40,
+                'discount' => 0, // 10 CHF
+            ],
+            [
+                'tokens' => 100,
+                'discount' => 0, // 25 CHF
+            ],
+            [
+                'tokens' => 200,
+                'discount' => 0.1, // 45 CHF instead of 50
+            ],
+            [
+                'tokens' => 400,
+                'discount' => 0.2, // 80 CHF instead of 100
+            ],
+            [
+                'tokens' => 1000,
+                'discount' => 0.4, // 150 CHF instead of 250
+            ],
+            [
+                'tokens' => 100_000,
+                'discount' => 0.98, // 500 CHF instead of 25k
+            ],
+        ];
+
+        $tokenPrice = 0.25;
+
+        foreach ($tokenPackages as $pkg) {
+            $tokens = $pkg['tokens'];
+            $discount = $pkg['discount'];
+            $stdPrice = $tokens * $tokenPrice;
+            $price = $stdPrice * (1 - $discount);
+            Article::create([
+                'type' => 'tokens-package',
+                'name' => "$tokens-tokens",
+                'description' => "$tokens jetons",
+                'currency' => 'CHF',
+                'std_price' => $stdPrice,
+                'price' => $price,
+            ]);
+        }
+    }
+}
