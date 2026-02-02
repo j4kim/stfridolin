4f2daa1e1000bf9676456f373e8a5139985bc735
Joaquim Perez <jivkim@gmail.com>
2026-02-02 19:12:02 +0100

    new Article model + migration + controller

---
+++ database/migrations/2026_02_02_180721_create_articles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('type');
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('currency');
+            $table->decimal('std_price', 8, 2);
+            $table->decimal('price', 8, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
