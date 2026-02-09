7014869b677b2120a613282792b6d55ab37acf4e
Joaquim Perez <jivkim@gmail.com>
2026-02-09 19:02:40 +0100

    php artisan make:filament-resource Payment

---
+++ app/Filament/Resources/Payments/Tables/PaymentsTable.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Filament\Resources\Payments\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class PaymentsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('guest.name')
+                    ->searchable(),
+                TextColumn::make('stripe_id')
+                    ->searchable(),
+                TextColumn::make('stripe_status')
+                    ->searchable(),
+                TextColumn::make('purpose')
+                    ->searchable(),
+                TextColumn::make('amount')
+                    ->numeric()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                ViewAction::make(),
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
