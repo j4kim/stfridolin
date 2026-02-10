0503b0c8a74913640631dcba2035e412668d12c1
Joaquim Perez <jivkim@gmail.com>
2026-02-10 21:57:49 +0100

    feat(ListVouchers): a custom action to create many vouchers

--- app/Filament/Resources/Vouchers/Pages/ListVouchers.php
+++ app/Filament/Resources/Vouchers/Pages/ListVouchers.php
@@ -3,8 +3,16 @@
 namespace App\Filament\Resources\Vouchers\Pages;
 
 use App\Filament\Resources\Vouchers\VoucherResource;
+use App\Models\Voucher;
+use Database\Factories\VoucherFactory;
+use Filament\Actions\Action;
 use Filament\Actions\CreateAction;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Support\Enums\Width;
+use Illuminate\Database\Eloquent\Model;
 
 class ListVouchers extends ListRecords
 {
@@ -13,7 +21,25 @@ class ListVouchers extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            Action::make('Créer')
+                ->schema([
+                    Select::make('article_id')
+                        ->relationship('article', 'description')
+                        ->required(),
+                    TextInput::make('quantity')
+                        ->label("Quantité")
+                        ->numeric()
+                        ->required()
+                        ->default(10),
+                ])
+                ->action(function (array $data) {
+                    Voucher::factory($data['quantity'])->create(['article_id' => $data['article_id']]);
+                    Notification::make()
+                        ->title("$data[quantity] codes créés")
+                        ->success()
+                        ->send();
+                })
+                ->modalWidth(Width::Medium),
         ];
     }
 }
