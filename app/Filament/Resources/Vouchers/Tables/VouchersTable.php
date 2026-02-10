dcab14d40b16c7031e4e74ace0d4716bb031683d
Joaquim Perez <jivkim@gmail.com>
2026-02-10 21:41:00 +0100

    new VoucherResource

---
+++ app/Filament/Resources/Vouchers/Tables/VouchersTable.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Filament\Resources\Vouchers\Tables;
+
+use App\Filament\Tools\ColumnTools;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class VouchersTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                ...ColumnTools::systemColumns(),
+                TextColumn::make('article.description')
+                    ->searchable(),
+                TextColumn::make('guest.name')
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                ViewAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
