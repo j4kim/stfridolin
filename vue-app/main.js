b3b77b8d7d3400b831c16ee49a4c8ef6f2b09544
Joaquim Perez <jivkim@gmail.com>
2026-02-14 00:51:08 +0100

    why broadcasting was imported in main.js ?? 192 -> 130 kB

--- vue-app/main.js
+++ vue-app/main.js
@@ -3,7 +3,6 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "@/App.vue";
 import router from "@/router";
-import "@/broadcasting";
 
 const app = createApp(App);
 
