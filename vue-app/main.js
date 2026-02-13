f44f4c9fab2b73ef0908e8da7b673d66ac5a02a1
Joaquim Perez <jivkim@gmail.com>
2026-02-14 00:40:17 +0100

    optimize(main, boxing, Ring): remove imports in main.js. Import frames from new initializeFighters in boxing store, call it from Ring 286 -> 192 kB

--- vue-app/main.js
+++ vue-app/main.js
@@ -1,21 +1,13 @@
 import "@/css/app.css";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import { importFrames } from "@/boxing/utils";
 import App from "@/App.vue";
 import router from "@/router";
 import "@/broadcasting";
-import { useClockStore } from "./stores/clock";
-import { useMainStore } from "./stores/main";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
-if (useMainStore().user) {
-    useClockStore().startClock();
-    await importFrames();
-}
 
 app.mount("#app");
