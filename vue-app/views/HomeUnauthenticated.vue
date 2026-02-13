9762dde764cf0a6aac1b3443cdb0a827bfcee459
Joaquim Perez <jivkim@gmail.com>
2026-02-13 11:59:36 +0100

    refactor: a new PublicLayout component that shows titleSvg and will render footer

--- vue-app/views/HomeUnauthenticated.vue
+++ vue-app/views/HomeUnauthenticated.vue
@@ -1,15 +1,14 @@
 <script setup>
-import titleSvg from "@/title.svg";
+import PublicLayout from "@/components/PublicLayout.vue";
 </script>
 
 <template>
-    <div class="mx-auto max-w-xl p-8">
-        <img class="mx-auto max-w-xl p-12" :src="titleSvg" />
+    <PublicLayout>
         <div class="prose lg:prose-lg text-white">
             <p>
                 Bienvenue sur la page officielle du PMU de soutien de la
                 Saint-Fridolin.
             </p>
         </div>
-    </div>
+    </PublicLayout>
 </template>
