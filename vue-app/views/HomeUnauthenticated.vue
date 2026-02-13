b47d347a21e50513c7ac0a14f852ff5a6e0026d2
Joaquim Perez <jivkim@gmail.com>
2026-02-13 12:14:53 +0100

    content(HomeUnauthenticated)

--- vue-app/views/HomeUnauthenticated.vue
+++ vue-app/views/HomeUnauthenticated.vue
@@ -1,13 +1,31 @@
 <script setup>
 import PublicLayout from "@/components/PublicLayout.vue";
+import Button from "@/components/ui/button/Button.vue";
 </script>
 
 <template>
     <PublicLayout>
-        <div class="prose lg:prose-lg text-white">
+        <div class="prose lg:prose-lg dark:prose-invert text-white">
             <p>
                 Bienvenue sur la page officielle du PMU de soutien de la
-                Saint-Fridolin.
+                Saint-Fridolin, qui aura lieu le 6 mars 2026 au Bikini Test. Une
+                soirée au profit d'Estelle Zamme agrémentée de plein de jeux de
+                pari rigolos. Votre participation financière contribuera aux
+                activités artistiques du collectif, notamment à financer la
+                tournée 2026 et les prochaines sorties musicales.
+            </p>
+            <p>
+                Vous pouvez valider votre inscription en cliquant sur le bouton
+                ci-dessous et payer les frais de 30 CHF par personne.
+            </p>
+            <p>
+                <RouterLink :to="{ name: 'registration-payment' }">
+                    <Button class="w-full" size="lg"> Inscription </Button>
+                </RouterLink>
+            </p>
+            <p>
+                Informations légales et conditions de participation disponibles
+                ci-dessous.
             </p>
         </div>
     </PublicLayout>
