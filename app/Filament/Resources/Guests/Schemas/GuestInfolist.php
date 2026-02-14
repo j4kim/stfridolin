ae2ea1261695142761cf9aed8d125d89be2f08c5
Joaquim Perez <jivkim@gmail.com>
2026-02-14 18:33:29 +0100

    feat(GuestInfolist): add stripe_customer_id and a button to create customer if it is missing

--- app/Filament/Resources/Guests/Schemas/GuestInfolist.php
+++ app/Filament/Resources/Guests/Schemas/GuestInfolist.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Resources\Guests\Schemas;
 
 use App\Filament\Tools\EntryTools;
+use App\Models\Guest;
+use Filament\Actions\Action;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Schemas\Schema;
 
@@ -21,6 +23,14 @@ class GuestInfolist
                         ->numeric(),
                     TextEntry::make('points')
                         ->numeric(),
+                    TextEntry::make('stripe_customer_id')
+                        ->belowLabel(function (?string $state) {
+                            if ($state) return null;
+                            return Action::make('create stripe customer')
+                                ->action(function (Guest $guest) {
+                                    $guest->createStripeCustomer()->save();
+                                });
+                        }),
                 ]),
             ]);
     }
