4d227b819a6fe58d99a2b24ce581157f49616c70
Joaquim Perez <jivkim@gmail.com>
2026-02-16 23:24:21 +0100

    feat(GuestPage): links to new route Spend, receive tokens or points as currency parameter

---
+++ vue-app/views/Spend.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Layout from "@/components/Layout.vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+</script>
+
+<template>
+    <Layout>
+        <h2 class="my-2 px-4 font-bold">
+            Dépenser des {{ route.params.currency }}
+        </h2>
+        <div class="px-4"></div>
+    </Layout>
+</template>
