3a93091439a4385fb3302455185f3d59928b4dd5
Joaquim Perez <jivkim@gmail.com>
2026-02-18 00:22:37 +0100

    ux(FullPageLoading): show a Spinner, lazy loaded itself to keep main bundle light

--- vue-app/views/FullPageLoading.vue
+++ vue-app/views/FullPageLoading.vue
@@ -1,5 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { defineAsyncComponent } from "vue";
+
+const Spinner = defineAsyncComponent(
+    () => import("@/components/ui/spinner/Spinner.vue"),
+);
+</script>
 
 <template>
-    <div class="flex h-dvh items-center justify-center">Chargement</div>
+    <div class="flex h-dvh flex-col items-center justify-center gap-2">
+        <Spinner class="size-10" />
+        <div>Chargement</div>
+    </div>
 </template>
