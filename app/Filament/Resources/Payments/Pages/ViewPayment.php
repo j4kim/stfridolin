48f9516dab1d1bebeb9a8033cb8f8f3896eeb480
Joaquim Perez <jivkim@gmail.com>
2026-02-19 14:06:25 +0100

    feat(ViewPayment): a button to manually confirm non Stripe Payments

--- app/Filament/Resources/Payments/Pages/ViewPayment.php
+++ app/Filament/Resources/Payments/Pages/ViewPayment.php
@@ -2,8 +2,12 @@
 
 namespace App\Filament\Resources\Payments\Pages;
 
+use App\Enums\PaymentMethod;
 use App\Enums\PaymentPurpose;
+use App\Enums\PaymentStatus;
 use App\Filament\Resources\Payments\PaymentResource;
+use App\Models\Payment;
+use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\EditAction;
 use Filament\Forms\Components\Select;
@@ -19,6 +23,11 @@ class ViewPayment extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('confirm')
+                ->hidden(fn(Payment $payment) => $payment->method === PaymentMethod::Stripe)
+                ->visible(fn(Payment $payment) => $payment->status !== PaymentStatus::succeeded)
+                ->requiresConfirmation()
+                ->action(fn(Payment $payment) => $payment->update(['status' => PaymentStatus::succeeded])),
             EditAction::make()
                 ->schema([
                     Select::make('purpose')->options(PaymentPurpose::class),
