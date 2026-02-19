cef4c472a2ec71a8db345f125e162ca985442cd2
Joaquim Perez <jivkim@gmail.com>
2026-02-19 13:50:34 +0100

    feat(ViewPayment): button to update puropse, amount and description

--- app/Filament/Resources/Payments/Pages/ViewPayment.php
+++ app/Filament/Resources/Payments/Pages/ViewPayment.php
@@ -2,9 +2,14 @@
 
 namespace App\Filament\Resources\Payments\Pages;
 
+use App\Enums\PaymentPurpose;
 use App\Filament\Resources\Payments\PaymentResource;
 use Filament\Actions\DeleteAction;
+use Filament\Actions\EditAction;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Pages\ViewRecord;
+use Filament\Support\Enums\Width;
 use Illuminate\Contracts\Support\Htmlable;
 
 class ViewPayment extends ViewRecord
@@ -14,6 +19,13 @@ class ViewPayment extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
+            EditAction::make()
+                ->schema([
+                    Select::make('purpose')->options(PaymentPurpose::class),
+                    TextInput::make('amount')->numeric(),
+                    TextInput::make('description'),
+                ])
+                ->modalWidth(Width::Medium),
             DeleteAction::make(),
         ];
     }
