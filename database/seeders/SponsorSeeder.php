f06429138c23bf67aa34549edaedd50efd31ea75
Joaquim Perez <jivkim@gmail.com>
2026-02-20 01:25:38 +0100

    SponsorSeeder: seed 3 sponsors

--- database/seeders/SponsorSeeder.php
+++ database/seeders/SponsorSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Sponsor;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,17 @@ class SponsorSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Sponsor::create([
+            "name" => "La Semeuse",
+            "logo_url" => "https://www.lasemeuse.ch/static/version1770816718/frontend/Mgs/lasemeuse/fr_FR/images/logo.svg",
+        ]);
+        Sponsor::create([
+            "name" => "Christen Delicatessen",
+            "logo_url" => "https://christen-delicatessen.ch/wp-content/uploads/2021/05/logo_blanc.svg",
+        ]);
+        Sponsor::create([
+            "name" => "Coyote",
+            "logo_url" => asset("logos/coyote.png"),
+        ]);
     }
 }
