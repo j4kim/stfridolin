a9cee2ed6815a4b337ff39def10d66f6ff682059
Joaquim Perez <jivkim@gmail.com>
2026-02-20 18:09:05 +0100

    feat(Sponsor): new cached static method to return all logos

--- app/Models/Sponsor.php
+++ app/Models/Sponsor.php
@@ -4,10 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Storage;
 
 class Sponsor extends Model
 {
+    protected static function booted(): void
+    {
+        static::saved(fn() => Cache::forget("sponsors"));
+        static::deleted(fn() => Cache::forget("sponsors"));
+    }
+
     protected $appends = ['logo_url'];
 
     protected function logoUrl(): Attribute
@@ -15,5 +23,10 @@ class Sponsor extends Model
         return Attribute::make(
             get: fn() => Storage::url($this->logo_path),
         );
+    }
+
+    public static function cached(): Collection
+    {
+        return Cache::remember("sponsors", 60 * 60, fn() => Sponsor::all());
     }
 }
