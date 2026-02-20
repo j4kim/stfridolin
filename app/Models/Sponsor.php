fcc515dcdd9d9796fa319241685758a1b80d5f6c
Joaquim Perez <jivkim@gmail.com>
2026-02-20 17:56:24 +0100

    feat(Sponsor): add logo_url

--- app/Models/Sponsor.php
+++ app/Models/Sponsor.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Sponsor extends Model
 {
-    //
+    protected $appends = ['logo_url'];
+
+    protected function logoUrl(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => Storage::url($this->logo_path),
+        );
+    }
 }
