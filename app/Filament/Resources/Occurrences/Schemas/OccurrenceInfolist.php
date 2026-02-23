9cc7ec2f481991f2af1cbd9d88efd0cf0ef0e351
Joaquim Perez <jivkim@gmail.com>
2026-02-23 11:21:05 +0100

    OccurrencesInfolist: link to game

--- app/Filament/Resources/Occurrences/Schemas/OccurrenceInfolist.php
+++ app/Filament/Resources/Occurrences/Schemas/OccurrenceInfolist.php
@@ -2,7 +2,9 @@
 
 namespace App\Filament\Resources\Occurrences\Schemas;
 
+use App\Filament\Resources\Games\GameResource;
 use App\Filament\Tools\EntryTools;
+use App\Models\Game;
 use Filament\Infolists\Components\KeyValueEntry;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Schemas\Schema;
@@ -17,6 +19,9 @@ class OccurrenceInfolist
 
                 EntryTools::compactSection()
                     ->schema([
+                        TextEntry::make('game')
+                            ->url(fn(Game $state): string => GameResource::getUrl('view', ['record' => $state]))
+                            ->formatStateUsing(fn(Game $state): string => $state->name),
                         TextEntry::make('title'),
                         TextEntry::make('start_at')->dateTime('d.m.Y H:i'),
                         TextEntry::make('end_at')->dateTime('d.m.Y H:i'),
