c9ec1d4b8072460ddcb805339eb98c58767d65f8
Joaquim Perez <jivkim@gmail.com>
2026-02-23 10:28:45 +0100

    migration(occurrences)

--- database/migrations/2026_02_23_102650_create_occurrences_table.php
+++ database/migrations/2026_02_23_102650_create_occurrences_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('occurrences', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('title')->nullable();
+            $table->dateTime('start_at')->nullable();
+            $table->dateTime('end_at')->nullable();
+            $table->json('meta')->nullable();
         });
     }
 
