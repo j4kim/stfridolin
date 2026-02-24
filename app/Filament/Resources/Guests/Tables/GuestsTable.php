474551eff12cb1dca879c4e28c81568cd794e0f5
Joaquim Perez <jivkim@gmail.com>
2026-02-24 14:52:51 +0100

    default sort for guests, movements and payments tables

--- app/Filament/Resources/Guests/Tables/GuestsTable.php
+++ app/Filament/Resources/Guests/Tables/GuestsTable.php
@@ -94,6 +94,7 @@ class GuestsTable
                 BulkActionGroup::make([
                     DeleteBulkAction::make(),
                 ]),
-            ]);
+            ])
+            ->defaultSort('created_at', 'desc');
     }
 }
