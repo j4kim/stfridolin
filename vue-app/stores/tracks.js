978df7030067e1fdb6ed0748819deaf4aee58e8d
Joaquim Perez <jivkim@gmail.com>
2026-02-24 15:26:24 +0100

    refactor,feat(Search, tracks): move search logic in tracks store, in order to keep state after a navigation (if guest is short on tokens)

---
+++ vue-app/stores/tracks.js
@@ -0,0 +1,39 @@
+import { defineStore } from "pinia";
+import { computed, ref } from "vue";
+import { groupBy, keyBy } from "lodash-es";
+import { watchDebounced } from "@vueuse/core";
+import { api } from "@/api";
+
+export const useTracksStore = defineStore("tracks", () => {
+    const searchQuery = ref("");
+
+    const searchResults = ref(null);
+
+    async function searchTracks() {
+        searchResults.value = await api("spotify.search-tracks")
+            .params({ q: searchQuery.value })
+            .get();
+    }
+
+    watchDebounced(searchQuery, searchTracks, { debounce: 500 });
+
+    async function searchMore() {
+        const data = await api("spotify.search-tracks")
+            .params({
+                q: searchQuery.value,
+                offset: searchResults.value.offset + 10,
+            })
+            .get();
+        searchResults.value = {
+            ...data,
+            items: searchResults.value.items.concat(data.items),
+        };
+    }
+
+    return {
+        searchQuery,
+        searchResults,
+        searchTracks,
+        searchMore,
+    };
+});
