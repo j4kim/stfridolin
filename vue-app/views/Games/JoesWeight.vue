f8edf8c7f6b6d38b91fc099d23c08cf0d3c487cf
Joaquim Perez <jivkim@gmail.com>
2026-02-28 23:26:30 +0100

    feat(JoesWeight): ValidationDrawer

--- vue-app/views/Games/JoesWeight.vue
+++ vue-app/views/Games/JoesWeight.vue
@@ -5,6 +5,7 @@ import Field from "@/components/ui/field/Field.vue";
 import Input from "@/components/ui/input/Input.vue";
 import Label from "@/components/ui/label/Label.vue";
 import Spinner from "@/components/ui/spinner/Spinner.vue";
+import ValidationDrawer from "@/components/ValidationDrawer.vue";
 import { useGamesStore } from "@/stores/games";
 import { onUnmounted, ref } from "vue";
 
@@ -16,14 +17,19 @@ gamesStore.fetchGame();
 
 onUnmounted(() => (gamesStore.gameName = null));
 
-const guess = ref(null);
+const grams = ref(null);
+const submitting = ref(false);
+
+async function submit() {
+    console.log(grams.value);
+}
 </script>
 
 <template>
     <Layout>
         <h2 class="my-2 space-x-1 px-4">{{ gamesStore.game?.title }}</h2>
 
-        <form class="space-y-4 px-4">
+        <form class="space-y-6 px-4">
             <p class="text-sm">
                 Tu l'as vu notre beau dauphin à facette ? Devine son poids. La
                 personne la plus proche décroche le gros lot.
@@ -34,15 +40,29 @@ const guess = ref(null);
                 <div class="flex items-center justify-center gap-2">
                     <Input
                         type="number"
-                        class="h-14 text-center text-xl"
-                        v-model="guess"
+                        class="h-10 text-center text-lg"
+                        v-model="grams"
                         required
                     />
                     grammes
                 </div>
             </Field>
 
-            <Button type="submit" size="lg" class="w-full">Valider</Button>
+            <ValidationDrawer
+                :title="`Valider ${grams} g&nbsp;?`"
+                :action="submit"
+                articleName="guess-joes-weight"
+            >
+                <template #trigger>
+                    <Button
+                        :disabled="submitting || !grams"
+                        size="lg"
+                        class="w-full"
+                    >
+                        Placer le pari
+                    </Button>
+                </template>
+            </ValidationDrawer>
         </form>
 
         <Spinner v-if="gamesStore.fetching" class="m-4"></Spinner>
