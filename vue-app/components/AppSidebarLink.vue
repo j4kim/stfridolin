4feef8f7bbb19fb68af73fec83a4be6ea0e01804
Joaquim Perez <jivkim@gmail.com>
2026-03-01 15:12:23 +0100

    refactor(AppSidebarLink): use template approach for custom RouterLink

--- vue-app/components/AppSidebarLink.vue
+++ vue-app/components/AppSidebarLink.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { useLink } from "vue-router";
 import SidebarMenuButton from "./ui/sidebar/SidebarMenuButton.vue";
 import { useSidebar } from "./ui/sidebar";
 
@@ -13,22 +12,33 @@ const props = defineProps({
     to: Object | String,
     external: Boolean,
 });
-
-const { isExactActive, navigate, href } = useLink(props);
-
-function onClick() {
-    navigate();
-    sidebar.setOpenMobile(false);
-}
 </script>
 
 <template>
-    <SidebarMenuButton as-child :is-active="isExactActive">
+    <SidebarMenuButton v-if="external" as-child>
         <a v-if="external" v-bind="$attrs" :href="to">
             <slot />
         </a>
-        <a v-else v-bind="$attrs" :href="href" @click.prevent="onClick">
-            <slot />
-        </a>
     </SidebarMenuButton>
+    <RouterLink
+        v-else
+        :to="to"
+        custom
+        v-slot="{ isExactActive, href, navigate }"
+    >
+        <SidebarMenuButton as-child :is-active="isExactActive">
+            <a
+                v-bind="$attrs"
+                :href="href"
+                @click="
+                    ($e) => {
+                        navigate($e);
+                        sidebar.setOpenMobile(false);
+                    }
+                "
+            >
+                <slot />
+            </a>
+        </SidebarMenuButton>
+    </RouterLink>
 </template>
