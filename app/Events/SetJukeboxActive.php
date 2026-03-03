c43bb94c77e6a880d7a612e4a901d40d92d9eec5
Max <maxpiergiov@gmail.com>
2026-03-03 21:57:00 +0100

    Added field active to games, added listener to modification of game jukebox so that we broadcast an event when modified in filament

---
+++ app/Events/SetJukeboxActive.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class SetJukeboxActive implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(public bool $is_active) {}
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('set-jukebox-active'),
+        ];
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'SetJukeboxActive';
+    }
+}
