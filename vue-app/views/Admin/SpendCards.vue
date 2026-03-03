67316d9d7a6d7989f70978ec9346cc5e1e36437b
Joaquim Perez <jivkim@gmail.com>
2026-03-03 11:54:54 +0100

    feat(SpendCards): concatenate article cards with free spend cards

--- vue-app/views/Admin/SpendCards.vue
+++ vue-app/views/Admin/SpendCards.vue
@@ -1,50 +1,60 @@
 <script setup>
-import { api } from "@/api";
 import PrintableCards from "@/components/PrintableCards.vue";
 import QrCode from "@/components/QrCode.vue";
 import { useArticlesStore } from "@/stores/articles";
-import { icon } from "@/translate";
-import { computed, ref } from "vue";
+import { icon, tr } from "@/translate";
+import { computed } from "vue";
 import { useLink } from "vue-router";
 
 const articlesStore = useArticlesStore();
 
-const articles = computed(() => {
-    return articlesStore.articles
+const cards = computed(() => {
+    const articleCards = articlesStore.articles
         .filter((a) => a.type === "participation")
         .filter((a) => a.meta?.qrcodes)
-        .map((a) => {
-            const link = useLink({
-                to: {
-                    name: "spend",
-                    params: { currency: "tokens" },
-                    query: { article: a.name },
-                },
-            });
-            a.spendUrl = location.origin + link.href.value;
-            return a;
-        });
+        .map((a) => ({
+            ...a,
+            to: {
+                name: "spend",
+                params: { currency: "tokens" },
+                query: { article: a.name },
+            },
+            copies: a.meta.qrcodes,
+        }));
+    const freeSpendCards = ["tokens", "points"].map((currency) => ({
+        to: { name: "spend", params: { currency } },
+        description: "Dépense de " + tr(currency),
+        currency,
+        copies: 2,
+    }));
+    const all = articleCards.concat(freeSpendCards);
+    return all.map((c) => {
+        const { href } = useLink({ to: c.to });
+        c.url = location.origin + href.value;
+        return c;
+    });
 });
 </script>
 
 <template>
-    <PrintableCards :items="articles">
-        <template #item="article">
+    <PrintableCards :items="cards">
+        <template #item="card">
             <h1 class="text text-center font-bold">
-                {{ article.description }}
+                {{ card.description }}
             </h1>
             <h2
-                class="mt-2 flex items-center justify-center gap-1 text-xl opacity-50"
-                v-if="article.price"
+                class="mt-2 flex flex-col items-center gap-1 text-xl opacity-50"
             >
                 <component
-                    v-if="article.currency"
-                    :is="icon(article.currency)"
+                    v-if="card.currency"
+                    :is="icon(card.currency)"
                     class=""
                 />
-                -{{ article.price }} jetons
+                <span v-if="card.price">
+                    -{{ card.price }} {{ tr(card.currency) }}
+                </span>
             </h2>
-            <QrCode :value="article.spendUrl" class="mx-auto" :width="120" />
+            <QrCode :value="card.url" class="mx-auto" :width="120" />
         </template>
     </PrintableCards>
 </template>
