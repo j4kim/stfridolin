926a8041671c6d667ba20d2f15b06aaba55e232e
Joaquim Perez <jivkim@gmail.com>
2026-03-03 14:55:10 +0100

    Guest: cast type using new GuestType enum

---
+++ app/Enums/GuestType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Enums;
+
+use Filament\Support\Colors\Color;
+use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasLabel;
+use Illuminate\Contracts\Support\Htmlable;
+
+enum GuestType: string implements HasLabel, HasColor
+{
+    case Guest = 'guest';
+    case VIP = 'vip';
+    case Volunteer = 'volunteer';
+
+    public function getLabel(): string | Htmlable | null
+    {
+        return ucfirst(__($this->value));
+    }
+
+    public function getColor(): string | array | null
+    {
+        return match ($this) {
+            self::Guest => Color::Lime,
+            self::VIP => Color::Cyan,
+            self::Volunteer => Color::Emerald,
+            default => Color::Slate,
+        };
+    }
+}
