15e55bfda49721222a99390fb39690487613ad46
Joaquim Perez <jivkim@gmail.com>
2026-03-04 14:29:04 +0100

    feat(Controls): toggle playback button

--- vue-app/boxing/Controls.vue
+++ vue-app/boxing/Controls.vue
@@ -3,6 +3,7 @@ import { api } from "@/api";
 import Button from "@/components/ui/button/Button.vue";
 import { useFightStore } from "@/stores/fight";
 import { useSpotifyStore } from "@/stores/spotify";
+import { Pause, Play } from "lucide-vue-next";
 
 const fightStore = useFightStore();
 const spotify = useSpotifyStore();
@@ -18,7 +19,7 @@ async function vote(side) {
 </script>
 
 <template>
-    <div class="flex gap-2">
+    <div class="flex flex-wrap gap-2">
         <Button size="sm" variant="outline" @click="vote('left')">
             Vote left
         </Button>
@@ -33,6 +34,10 @@ async function vote(side) {
         </Button>
         <Button size="sm" variant="outline" @click="spotify.skipToNext">
             Skip to next track
+        </Button>
+        <Button size="sm" variant="outline" @click="spotify.toggle">
+            <component :is="spotify.playback?.is_playing ? Pause : Play" />
+            {{ spotify.playback?.is_playing ? "Pause" : "Play" }}
         </Button>
     </div>
 </template>
